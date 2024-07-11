7acbd94d4d02e67807f4ed83433e7f574aefc0b1
BaKaLLaNoT <allakacbn@gmail.com>
2024-07-11 23:01:30 +0200

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { configOptions } from './config/config-options';
+
+@Module({
+  imports: [ConfigModule.forRoot(configOptions)],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
